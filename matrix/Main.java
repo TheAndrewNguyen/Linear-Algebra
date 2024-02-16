081f1fab8d361c1a8a261cb8575991b69e853e4c
TheAndrewNguyen <andrewbvietnguyen@gmail.com>
2024-02-16 10:47:09 -0800

    commit before computer dies

--- matrix/Main.java
+++ matrix/Main.java
@@ -4,8 +4,11 @@ package matrix;
 
 public class Main {
     public static void main(String[] args){  
-        Matrix test = new Matrix(2, 3);
-        test = RowOperations.swap(test, 1, 2); 
+        Matrix test = new Matrix(2, 2); 
+        test = MatrixGenerator.set_1(test);
+        test.print_matrix();
 
+        test = RowOperations.scale(test, 0, 5); 
+        test.print_matrix();
     }   
 }
